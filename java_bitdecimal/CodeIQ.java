9928a6c8f6fcb5bc77ada75baf3f757ea464599b
elzzup <guild0105@gmail.com>
2016-01-09 13:02:04 +0900

    Pinch hanger

---
+++ java_bitdecimal/CodeIQ.java
@@ -0,0 +1,12 @@
+import java.math.*;
+class CodeIQ{
+public static void main(String[] args){
+    BigDecimal pointOne = new BigDecimal("0.1"); 
+    BigDecimal sum = new BigDecimal(0);
+    
+      for(int i=0;i<10;i++){
+      sum = sum.add(pointOne);
+    }
+    System.out.println("ans:"+ sum);
+    }
+}
